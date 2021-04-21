bff5533929b5a61d3d5262b64a839b4d19abbd83
Mariano <marianostanco4@gmail.com>
2021-04-20 21:56:55 -0300

    se realiza commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Exercises from '../pages/Exercises';
+import ExercisesNew from '../pages/ExercisesNew';
+import NotFound from '../pages/NotFound'
+import Hooks from './Hooks2'
+
+
+const App = () => (
+    <Router>
+        <Switch>
+            <Route exact path='/exercise' component={Exercises} />
+            <Route exact path='/exercise/new' component={ExercisesNew} />
+            <Route exact path='/hooks' component={Hooks} />
+            <Route component={NotFound} />
+        </Switch>
+    </Router>
+
+)
+
+export default App
